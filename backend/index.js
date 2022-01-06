b9ba4c64249a5a9e2129cd786e948b5b862cc639
Stefan <lazeastefan@gmail.com>
2022-01-07 00:26:24 +0200

    Update back

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,9 @@ const dotenv = require('dotenv');
 const uploader = require('express-fileupload');
 const cors = require('cors');
 const app = express();
-
+const path = require('path');
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded());
 app.use(uploader());
 app.use(cors());
 
@@ -17,11 +17,14 @@ dotenv.config();
 const PORT = process.env.PORT;
 model.sequelize.sync();
 
-app.use('/', express.static('../front/build'));
+app.use(express.static('../front/build'));
 
 app.use('/api', routes);
 
-
+app.get("*", (req, res) => {
+    console.log('aici', path.join(__dirname, "../front/public/", "index.html"))
+    res.sendFile(path.join(__dirname, "../front/build", 'index.html'))
+});
 app.listen(PORT, () => {
     console.log(`Server started on http://localhost:${PORT}`);
 })
