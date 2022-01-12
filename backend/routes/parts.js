898278065e7178d4f99d5b16d59aa4ecd127de09
Stefan <lazeastefan@gmail.com>
2022-01-12 07:57:02 +0200

    Save part route change

--- backend/routes/parts.js
+++ backend/routes/parts.js
@@ -5,6 +5,6 @@ const { authorizeGoogle } = require('../services/authorize');
 // router.get("/", authorizeGoogle, PartsController.getAllParts);
 router.get("/", PartsController.getAllParts);
 
-router.post("/", PartsController.savePart);
+router.post("/savePart", PartsController.savePart);
 
 module.exports = router;
