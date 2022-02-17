a42784d85d3b10def87e1d28f65699c2ca12360a
Stefan <lazeastefan@gmail.com>
2022-02-17 23:33:39 +0200

    Customize stepper

--- front/src/pages/home/Home.js
+++ front/src/pages/home/Home.js
@@ -1,8 +1,8 @@
 import React, { useRef, useState } from 'react';
-import _ from "lodash";
-
+import SettingsIcon from '@mui/icons-material/Settings';
+import GroupAddIcon from '@mui/icons-material/GroupAdd';
+import VideoLabelIcon from '@mui/icons-material/VideoLabel';
 import Button from '@mui/material/Button';
-import Input from '@mui/material/Input';
 
 import CustomStepper from '../../components/stepper/Stepper'
 
@@ -13,8 +13,11 @@ export const Home = (props) => {
     const inputRef = useRef();
     const [imgSrc, setImgSrc] = useState("https://via.placeholder.com/300")
     const [uploadFileName, setUploadFileName] = useState("")
-    const [step, setStep] = useState(0);
-    const steps = ['Asigurare', 'Rovigneta', 'ITP'];
+    const [step, setStep] = useState(-1);
+    const stepsConfig = [
+        { label: 'ITP', icon: <SettingsIcon /> },
+        { label: 'Asigurare', icon: <GroupAddIcon /> },
+        { label: 'Rovigneta', icon: <VideoLabelIcon /> }];
 
 
     const handleUpload = (e) => {
@@ -28,7 +31,19 @@ export const Home = (props) => {
         setImgSrc(URL.createObjectURL(fileUploaded))
         console.log(fileUploaded)
     }
-    /** */
+
+    const nextStep = () => {
+        console.log(step, stepsConfig.length)
+        if (step < stepsConfig.length - 1) {
+            setStep((prev) => prev + 1);
+        }
+    }
+
+    const prevStep = () => {
+        if (step > -1) {
+            setStep((prev) => prev - 1);
+        }
+    }
     return (
         <div className="home-page">
             <img
@@ -43,7 +58,19 @@ export const Home = (props) => {
                     Upload
                 </Button>
             </label>
-            <CustomStepper currentStep={step} steps={steps} />
+
+            <div className='steps-header'>
+                <Button variant="contained" onClick={prevStep}>
+                    Prev
+                </Button>
+                <Button variant="contained" onClick={nextStep}>
+                    Next
+                </Button>
+            </div>
+
+            <div className='step-container'>
+                <CustomStepper currentStep={step} steps={stepsConfig} onStepClick={(item) => console.log(item)} />
+            </div>
 
         </div>
     );
