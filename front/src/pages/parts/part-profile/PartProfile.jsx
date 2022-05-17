d1776e736347be126036c84b79a4d0a1ec767e47
Stefan <lazeastefan@gmail.com>
2022-05-17 18:16:43 +0300

    add details card

--- front/src/pages/parts/part-profile/PartProfile.jsx
+++ front/src/pages/parts/part-profile/PartProfile.jsx
@@ -1,17 +1,41 @@
 import React, { useEffect } from 'react';
-import { PageContainer } from '../../../components/page-container/PageContainer'
 import { useLocation } from 'react-router';
+
+import { PageContainer } from '../../../components/page-container/PageContainer'
+import { DetailCard } from '../components/detail-card/DetailCard';
+
+import { Grid } from '@mui/material';
+import _ from 'lodash'
+
+import styles from './PartProfile.module.scss'
 
 export const PartProfile = ({ ...props }) => {
     const { state } = useLocation();
+    const selectedPart = _.get(state, 'selectedPart', {});
 
     useEffect(() => {
-        console.log(state)
+        console.log(selectedPart)
     }, [state])
 
     return <PageContainer>
         <div>
-            hello
+            <span>Piesa ta, {selectedPart.code}</span>
+            <div className={styles.summariesGrid}>
+                <Grid container rowSpacing={4} spacing={{ xs: 1, sm: 3, md: 6 }} columns={{ xs: 1, sm: 8, md: 12 }}>
+                    <Grid item xs={1} sm={3} md={3}>
+                        <DetailCard />
+                    </Grid>
+                    <Grid item xs={1} sm={3} md={3}>
+                        <DetailCard />
+                    </Grid>
+                    <Grid item xs={1} sm={3} md={3}>
+                        <DetailCard />
+                    </Grid>
+                    <Grid item xs={1} sm={3} md={3}>
+                        <DetailCard />
+                    </Grid>
+                </Grid>
+            </div>
         </div>
     </PageContainer>
 }
